547374f8e30f9b05de0416a0894c2134f5a58697
DavidH29 <54407010+DavidH29@users.noreply.github.com>
2020-04-17 11:50:48 -0600

    Update CalculadoraImpuestos.java

--- src/com/MJMV/x00198319/CalculadoraImpuestos.java
+++ src/com/MJMV/x00198319/CalculadoraImpuestos.java
@@ -1,4 +1,21 @@
 package com.MJMV.x00198319;
 
 public static class CalculadoraImpuestos {
+
+    private double totalRenta;
+    private double totalISS;
+    private double totalAFP;
+
+    private CalculadoraImpuestos(){
+
+    }
+
+    public double calcularPago(Empleado){
+
+    }
+
+    public String mostrarTotales() {
+
+    }
+
 }
