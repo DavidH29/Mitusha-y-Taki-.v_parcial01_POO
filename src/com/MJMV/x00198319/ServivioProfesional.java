fe2b23ddd19958cfabb9a7a4cd864c3014aec805
María José Mendoza 00198319 <00198319@uca.edu.sv>
2020-04-17 11:41:15 -0600

    Update ServivioProfesional.java
    Update Servicio Profesional

--- src/com/MJMV/x00198319/ServivioProfesional.java
+++ src/com/MJMV/x00198319/ServivioProfesional.java
@@ -1,4 +1,17 @@
 package com.MJMV.x00198319;
 
 public class ServivioProfesional {
+    private int mesesContrato;
+
+    public ServivioProfesional(String nombre,String numero, double salario, int mesesContrato){
+
+    }
+
+    public int getMesesContrato() {
+        return mesesContrato;
+    }
+
+    public void setMesesContrato(int mesesContrato) {
+        this.mesesContrato = mesesContrato;
+    }
 }
