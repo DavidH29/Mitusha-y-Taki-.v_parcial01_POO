c82086d8a5f13251fce345b0d3638bee9c07f8e1
María José Mendoza 00198319 <00198319@uca.edu.sv>
2020-04-17 11:03:15 -0600

    Update
    Update Empleado

--- src/com/MJMV/x00198319/Empleado.java
+++ src/com/MJMV/x00198319/Empleado.java
@@ -1,4 +1,40 @@
 package com.MJMV.x00198319;
 
 public abstract class Empleado {
+    protected String Nombre;
+    protected String Puesto;
+    protected List<Documento>;
+    protected double Salario;
+
+    public  Empleado(String nombre, String puesto, double salario){
+
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getPuesto() {
+        return Puesto;
+    }
+
+    public getDocumento() List<Documento>{
+        
+    }
+
+    public void addDocumento(Documento){
+
+    }
+
+    public void removeDocumento(String){
+
+    }
+
+    public double getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(double salario) {
+        Salario = salario;
+    }
 }
