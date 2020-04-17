02657e9fb5a41c84a316eccbf415c3b6a1603ea7
DavidH29 <54407010+DavidH29@users.noreply.github.com>
2020-04-17 11:50:55 -0600

    Update Empleado.java

--- src/com/MJMV/x00198319/Empleado.java
+++ src/com/MJMV/x00198319/Empleado.java
@@ -20,8 +20,13 @@ public abstract class Empleado {
         return Puesto;
     }
 
+<<<<<<< HEAD
     public ArrayList<Documento> getDocumento(){
         
+=======
+    public getDocumento() List<Documento>{
+
+>>>>>>> fe2b23ddd19958cfabb9a7a4cd864c3014aec805
     }
 
     public void addDocumento(Documento){
